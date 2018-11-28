0b41bbc43f04c92d5ba1a2dff4a7d37fedaaa511
DANIELLE PARK <dpark1008@gmail.com>
2018-11-28 05:04:58 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
-def hello(array)
+def hi(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Jim", "John", "Peter"]) { |name| "Hi, #{name}" }
