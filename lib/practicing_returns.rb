c9a24e54c7651f3e83bfc20c369e8a714ebe647d
Brandon Liu <bliu88@live.com>
2019-04-05 19:13:29 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,23 @@
-require 'pry'
 
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
 
+# call your method here!
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
