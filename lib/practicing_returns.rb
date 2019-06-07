927402c10add912f20b9939b5c5854fb39b117e3
Connor Cowan <ccowan003@comcast.net>
2019-06-07 02:53:34 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,15 @@
+#run in termianl with ruby lib/practicing_returns.rb
 require 'pry'
 
 def hello(array)
   i = 0
+  collectin = []            #init new array to capture return values
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])       #add those elements to collection
     i += 1
   end
+  return collection
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
