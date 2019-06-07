3e2cde9124f526bcf1aa56fcca477221a9ec05bf
Connor Cowan <ccowan003@comcast.net>
2019-06-07 02:54:27 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,9 +1,9 @@
-#run in termianl with ruby lib/practicing_returns.rb
+#run in terminal with ruby lib/practicing_returns.rb
 require 'pry'
 
 def hello(array)
   i = 0
-  collectin = []            #init new array to capture return values
+  collection = []            #init new array to capture return values
   while i < array.length
     collection << yield(array[i])       #add those elements to collection
     i += 1
