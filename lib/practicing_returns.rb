351e15cc5456cad083ad86fb8e0d4a568c3ecde1
Haim D <haim11293@aol.com>
2019-09-11 14:07:23 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+# define the hello method to take in an array, set a counter  equal to 0, create an empty array to push in out yield returns, use a while loop to iterate through the array, yield to each iteration and push the yield return to the collection array, increment the counter by 1, return the updated collection array.
+
 def hello(array)
   i = 0
   collection = []
