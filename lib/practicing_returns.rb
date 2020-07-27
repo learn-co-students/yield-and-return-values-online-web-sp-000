1140d5475c7341b248da65f5fa6efc7330cd9090
Anya Brun <tsbrun@icloud.com>
2020-07-27 17:14:14 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
 def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
+  i = 0 # set up counter
+  collection = [] # set up empty array
+  while i < array.length # initiate while loop
+    collection << yield(array[i]) # yield element i of array, then store return value in empty array
+    i += 1 # increment counter var
   end
-  collection
+  collection # return collection of modified values
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
